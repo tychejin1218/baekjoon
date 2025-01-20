f1568960df387752d9900ab55a453506fbcd17c8
너나들이 <tychejin@naver.com>
2025-01-20 22:03:30 +0900

    Chore: 코딩테스트 연습
    문제_13223 : 소금 폭탁

--- src/fastcampus/part1/Problem13223.java
+++ src/fastcampus/part1/Problem13223.java
@@ -5,86 +5,72 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 /**
- * 단어공부
+ * 소금 폭탁
  * <p/>
- * https://www.acmicpc.net/problem/1157
+ * https://www.acmicpc.net/problem/13223
  */
-public class Problem1157 {
+public class Problem13223 {
 
   public static void main(String[] args) throws IOException {
 
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    String str = br.readLine().toLowerCase();
 
-    char[] charArr = str.toCharArray();
+    String[] current = br.readLine().split(":");
+    String[] target = br.readLine().split(":");
 
-    int[] countArr = new int[26];
+    int currentHour = Integer.parseInt(current[0]);
+    int currentMinute = Integer.parseInt(current[1]);
+    int currentSecond = Integer.parseInt(current[2]);
 
-    for (char c : charArr) {
-      countArr[c - 'a']++;
-    }
+    int targetHour = Integer.parseInt(target[0]);
+    int targetMinute = Integer.parseInt(target[1]);
+    int targetSecond = Integer.parseInt(target[2]);
 
-    int maxIdx = Integer.MIN_VALUE;
-    int maxCount = Integer.MIN_VALUE;
-    boolean isDuplicate = false;
+    int currentSecondAmount = currentHour * 3600 + currentMinute * 60 + currentSecond;
+    int targetSecondAmount = targetHour * 3600 + targetMinute * 60 + targetSecond;
 
-    for (int i = 0; i < countArr.length; i++) {
-      int count = countArr[i];
-      if (count > maxCount) {
-        maxCount = count;
-        maxIdx = i;
-        isDuplicate = false;
-      } else if (count == maxCount) {
-        isDuplicate = true;
-      }
-    }
+    int NeedSecondAmount = targetSecondAmount - currentSecondAmount;
+    if (NeedSecondAmount <= 0) NeedSecondAmount += 24 * 3600;
 
-    if (isDuplicate) {
-      System.out.println("?");
-    } else {
-      System.out.println(Character.toChars(maxIdx + 'A'));
-    }
+    int needHour = NeedSecondAmount / 3600;
+    int needMinute = (NeedSecondAmount % 3600) / 60;
+    int needSecond = NeedSecondAmount % 60;
+
+    System.out.println(String.format("%02d:%02d:%02d", needHour, needMinute, needSecond));
   }
 }
 
 /*
-import java.io.*
+import java.io.*;
 
 public class Main {
 
-  public static void main(String[] args) throws IOException {
+   public static void main(String[] args) throws IOException {
 
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    String str = br.readLine().toLowerCase();
 
-    char[] charArr = str.toCharArray();
+    String[] current = br.readLine().split(":");
+    String[] target = br.readLine().split(":");
 
-    int[] countArr = new int[26];
+    int currentHour = Integer.parseInt(current[0]);
+    int currentMinute = Integer.parseInt(current[1]);
+    int currentSecond = Integer.parseInt(current[2]);
 
-    for (char c : charArr) {
-      countArr[c - 'a']++;
-    }
+    int targetHour = Integer.parseInt(target[0]);
+    int targetMinute = Integer.parseInt(target[1]);
+    int targetSecond = Integer.parseInt(target[2]);
 
-    int maxIdx = Integer.MIN_VALUE;
-    int maxCount = Integer.MIN_VALUE;
-    boolean isDuplicate = false;
+    int currentSecondAmount = currentHour * 3600 + currentMinute * 60 + currentSecond;
+    int targetSecondAmount = targetHour * 3600 + targetMinute * 60 + targetSecond;
 
-    for (int i = 0; i < countArr.length; i++) {
-      int count = countArr[i];
-      if (count > maxCount) {
-        maxCount = count;
-        maxIdx = i;
-        isDuplicate = false;
-      } else if (count == maxCount) {
-        isDuplicate = true;
-      }
-    }
+    int NeedSecondAmount = targetSecondAmount - currentSecondAmount;
+    if (NeedSecondAmount <= 0) NeedSecondAmount += 24 * 3600;
 
-    if (isDuplicate) {
-      System.out.println("?");
-    } else {
-      System.out.println(Character.toChars(maxIdx + 'A'));
-    }
+    int needHour = NeedSecondAmount / 3600;
+    int needMinute = (NeedSecondAmount % 3600) / 60;
+    int needSecond = NeedSecondAmount % 60;
+
+    System.out.println(String.format("%02d:%02d:%02d", needHour, needMinute, needSecond));
   }
 }
 */
