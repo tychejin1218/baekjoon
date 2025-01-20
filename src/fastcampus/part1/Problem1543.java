2983b8827f7f990bb075903217eb7e11a19af3e7
너나들이 <tychejin@naver.com>
2025-01-20 21:21:43 +0900

    Chore: 코딩테스트 연습
    문제_1543 : 문서 검색

---
+++ src/fastcampus/part1/Problem1543.java
@@ -0,0 +1,54 @@
+package fastcampus.part1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * 문서 검색
+ * <p/>
+ * https://www.acmicpc.net/problem/1543
+ */
+public class Problem1543 {
+
+  public static void main(String[] args) throws IOException {
+
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    String target = br.readLine();
+    String word = br.readLine();
+
+    int count = 0;
+    int index = 0;
+
+    while ((index = target.indexOf(word, index)) != -1) {
+      count++;
+      index += word.length();
+    }
+
+    System.out.println(count);
+  }
+}
+
+/*
+import java.io.*;
+
+public class Main {
+
+  public static void main(String[] args) throws IOException {
+
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    String target = br.readLine();
+    String word = br.readLine();
+
+    int count = 0;
+    int index = 0;
+
+    while ((index = target.indexOf(word, index)) != -1) {
+      count++;
+      index += word.length();
+    }
+
+    System.out.println(count);
+  }
+}
+*/
