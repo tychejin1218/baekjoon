0d7d7775d84b9f25db3504c5fbfbc5bb18081e70
너나들이 <tychejin@naver.com>
2025-02-05 09:23:31 +0900

    Chore: 코딩테스트 연습
    문제_10250: ACM 호텔

---
+++ src/fastcampus/part1/Problem10250.java
@@ -0,0 +1,72 @@
+package fastcampus.part1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * ACM 호텔
+ * <p/>
+ * https://www.acmicpc.net/problem/10250
+ */
+public class Problem10250 {
+
+  public static void main(String[] args) throws IOException {
+
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int T = Integer.parseInt(br.readLine());
+
+    while (T-- > 0) {
+
+      String[] inputArr = br.readLine().split(" ");
+      int H = Integer.parseInt(inputArr[0]);
+      int W = Integer.parseInt(inputArr[1]);
+      int N = Integer.parseInt(inputArr[2]);
+
+      int checkCount = 1;
+      Loop1:
+      for (int i = 1; i <= W; i++) {
+        for (int j = 1; j <= H; j++) {
+          if (checkCount == N) {
+            System.out.println(j + String.format("%02d", i));
+            break Loop1;
+          }
+          checkCount++;
+        }
+      }
+    }
+  }
+}
+
+/*
+import java.io.*;
+
+public class Main {
+
+  public static void main(String[] args) throws IOException {
+
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int T = Integer.parseInt(br.readLine());
+
+    while (T-- > 0) {
+
+      String[] inputArr = br.readLine().split(" ");
+      int H = Integer.parseInt(inputArr[0]);
+      int W = Integer.parseInt(inputArr[1]);
+      int N = Integer.parseInt(inputArr[2]);
+
+      int checkCount = 1;
+      Loop1:
+      for (int i = 1; i <= W; i++) {
+        for (int j = 1; j <= H; j++) {
+          if (checkCount == N) {
+            System.out.println(j + String.format("%02d", i));
+            break Loop1;
+          }
+          checkCount++;
+        }
+      }
+    }
+  }
+}
+*/
